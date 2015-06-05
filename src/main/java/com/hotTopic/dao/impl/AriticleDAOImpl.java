3dbbdc628c6d0c35a1f82d99bfa63e975442c242
bingbinglao <bingbinglao@hengtiansoft.com>
2015-06-05 14:34:54 +0800

    test push

--- src/main/java/com/hotTopic/dao/impl/AriticleDAOImpl.java
+++ src/main/java/com/hotTopic/dao/impl/AriticleDAOImpl.java
@@ -8,7 +8,7 @@ import com.hotTopic.domain.Article;
 public class AriticleDAOImpl implements IArticleDAO {
 
 	/**
-	 * 取出今天所有的文章
+	 * 取出今天所有的文章 
 	 */
 	public List<Article> getTodayArticles() {
 		return null;
